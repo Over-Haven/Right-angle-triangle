1e6fd6130c6279b0d50a2e01f0389bac3823902b
Over-haven <tanushdas26@gmail.com>
2024-11-16 20:40:28 +0530

    hi

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 #take
-print("Half pyramid patter is start(*):")
-n = int(input("Enter the number of row:"))
-#outer
-for i in range(n):
+row = int(input("Please enter the total number of rows :"))
+number = 1 # 1
+
+
+print("Floyd's Triangle")
+#output
+for i in range(1, row + 1):
     #inner
-    for j in range(i + 1):
-        #display result
-        print("* ", end = "")
+    for j in range(i, i + 1):
+        #display
+        print(number,end = ' ')
+        number = number + 1
     print()
